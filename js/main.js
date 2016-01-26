5459e403f00278c2288e2c826521b8f10992a4ea
Wesley Aspinall <wra427@gmail.com>
2016-01-25 21:34:01 -0500

    practice with observables and data binding. Added a few buttons to demonstrate click binding

--- js/main.js
+++ js/main.js
@@ -2,4 +2,35 @@ import $ from 'jquery';
 import _ from 'underscore';
 import moment from 'moment';
 
-console.log('Hello, World');
+let ko = require('knockout');
+
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+
+  this.firstName = ko.observable("Bert");
+  this.lastName = ko.observable("Bertington");
+
+  this.fullName = ko.computed(function(){
+
+    return this.firstName() + " " + this.lastName();
+  }, this);
+
+  this.capitalizeLastName = function() {
+    let currentVal = this.lastName();
+    this.lastName(currentVal.toUpperCase());
+  };
+
+  this.lowercaseLastName = function() {
+    let currentVal = this.lastName();
+    this.lastName(currentVal.toLowerCase());
+  };
+
+  this.capitalizeFirstLetter = function() {
+    let currentVal = this.lastName();
+    this.lastName(currentVal.charAt(0).toUpperCase()+currentVal.slice(1));
+  };
+    
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
